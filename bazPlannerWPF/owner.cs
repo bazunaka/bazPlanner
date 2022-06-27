358ce293100d719e9b5ea3aad91e2a37590642f8
bazunaka <bazunaka32@gmail.com>
2022-06-27 23:18:55 +0300

    add first class

---
+++ bazPlannerWPF/owner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bazPlannerWPF
+{
+    class Owner
+    {
+        public String nameOwner;
+        public String passwordOwner;
+    }
+}
