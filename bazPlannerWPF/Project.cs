a545f5c2f27bf2e6659351dac28e9cd42e1e3681
bazunaka <bazunaka32@gmail.com>
2022-07-11 22:13:07 +0300

    generate list project

--- bazPlannerWPF/Project.cs
+++ bazPlannerWPF/Project.cs
@@ -1,19 +1,38 @@
-﻿namespace bazPlannerWPF
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Windows;
+using static bazPlannerWPF.DBConnect;
+
+namespace bazPlannerWPF
 {
     public class Project
     {
         public string nameProject;
         public string ownerProject;
         public string dateProject;
+        
 
-        public void InsertProject(string nameProject, string ownerProject, string dateProjec)
+        public void InsertProject(string nameProject, string ownerProject, string dateProject)
         {
             //SQL Insert into table
         }
 
-        public void SelectProject()
+        static public void SelectProject()
         {
-            //SQL Select from table
+            command = new SQLiteCommand(connection)
+            {
+                CommandText = $"SELECT name_project FROM project WHERE owner_project=2"
+            };
+            SQLiteDataReader reader = command.ExecuteReader();
+            //List<string> listProject = new List<string>();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    ((MainWindow)Application.Current.MainWindow).listElements.Items.Add(reader[0].ToString());
+                }
+            }
         }
     }
 }
--- bazPlannerWPF/auth.xaml.cs
+++ bazPlannerWPF/auth.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using static bazPlannerWPF.Owner;
+using static bazPlannerWPF.Project;
 
 namespace bazPlannerWPF
 {
@@ -13,6 +15,8 @@ namespace bazPlannerWPF
         private void AuthToDB(object sender, RoutedEventArgs e)
         {
             ((MainWindow)Application.Current.MainWindow).labelAuth.Content =  Auth(TextBoxUser.Text, TextBoxPassword.Text);
+            SelectProject();
+            //((MainWindow)Application.Current.MainWindow).listElements.Items.Add(SelectProject());
             Close();
         }
     }
