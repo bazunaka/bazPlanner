10304e585ba871eace9fab56e3945b0ead84a51c
bazunaka <bazunaka32@gmail.com>
2022-07-21 18:34:20 +0300

    new method

--- bazPlanner/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ bazPlanner/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -92,7 +92,7 @@ namespace bazPlanner {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/bazPlanner;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/bazPlanner;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
