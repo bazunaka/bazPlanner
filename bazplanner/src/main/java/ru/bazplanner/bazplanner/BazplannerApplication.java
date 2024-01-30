4552e2ba4b2919a616fbe9700c2454f80b1b717d
bazunaka-macbook <bazunaka32@gmail.com>
2024-01-30 17:09:34 +0300

    create app

---
+++ bazplanner/src/main/java/ru/bazplanner/bazplanner/BazplannerApplication.java
@@ -0,0 +1,13 @@
+package ru.bazplanner.bazplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazplannerApplication.class, args);
+	}
+
+}
